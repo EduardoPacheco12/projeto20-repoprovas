5d68b5f332db6724521bfa283bcef02234f592f0
EduardoPacheco12 <eduardooliveirapacheco131@gmail.com>
2022-09-18 21:56:43 -0300

    feat: add the tests of the test get creation routes

--- tests/test.test.ts
+++ tests/test.test.ts
@@ -1,3 +1,4 @@
+import e from 'express';
 import supertest from 'supertest';
 import app from '../src/app';
 import { client } from '../src/databases/postgres';
@@ -6,14 +7,14 @@ import { createTest } from './factories/testFactories';
 
 const agent = supertest(app);
 
-describe('Test POST /repoprovas/tests/create', () => {
-  beforeEach(async () => {
-    await client.$executeRaw`TRUNCATE TABLE "tests" RESTART IDENTITY`;
-  });
+beforeEach(async () => {
+  await client.$executeRaw`TRUNCATE TABLE "tests" RESTART IDENTITY`;
+});
 
+describe('Test POST /repoprovas/tests/create', () => {
   it('Should return 201 if registered an test in the correct format', async () => {
     const user = await createUser();
-    const register = await agent.post('/repoprovas/sign-up').send(user);
+    await agent.post('/repoprovas/sign-up').send(user);
     const loginUserBody = await loginUser(user);
     const login = await agent.post('/repoprovas/sign-in').send(loginUserBody);
     const token = login.text;
@@ -24,6 +25,17 @@ describe('Test POST /repoprovas/tests/create', () => {
       .send(test);
 
     expect(response.status).toEqual(201);
+  });
+
+  it('Should return 401 if the token is invalid', async () => {
+    const test = await createTest();
+    const token = '';
+    const response = await agent
+      .post('/repoprovas/tests/create')
+      .set({ Authorization: `Bearer ${token}` })
+      .send(test);
+
+    expect(response.status).toEqual(401);
   });
 
   it('Should return 404 if it does not find registered category, professor or discipline', async () => {
@@ -65,8 +77,60 @@ describe('Test POST /repoprovas/tests/create', () => {
 
     expect(response.status).toEqual(422);
   });
+});
 
-  afterAll(async () => {
-    await client.$disconnect();
+describe('Test GET /repoprovas/tests/disciplines', () => {
+  it('Should return 200 and a array of object if the get is done correctly', async () => {
+    const user = await createUser();
+    await agent.post('/repoprovas/sign-up').send(user);
+    const loginUserBody = await loginUser(user);
+    const login = await agent.post('/repoprovas/sign-in').send(loginUserBody);
+    const token = login.text;
+    const response = await agent
+      .get('/repoprovas/tests/disciplines')
+      .set({ Authorization: `Bearer ${token}` })
+      .send();
+    expect(response.status).toEqual(200);
+    expect(response.body).toBeInstanceOf(Array);
   });
+
+  it('Should return 401 if the token is invalid', async () => {
+    const token = '';
+    const response = await agent
+      .get('/repoprovas/tests/disciplines')
+      .set({ Authorization: `Bearer ${token}` })
+      .send();
+
+    expect(response.status).toEqual(401);
+  });
+});
+
+describe('Test GET /repoprovas/tests/teachers', () => {
+  it('Should return 200 and a array of object if the get is done correctly', async () => {
+    const user = await createUser();
+    await agent.post('/repoprovas/sign-up').send(user);
+    const loginUserBody = await loginUser(user);
+    const login = await agent.post('/repoprovas/sign-in').send(loginUserBody);
+    const token = login.text;
+    const response = await agent
+      .get('/repoprovas/tests/teachers')
+      .set({ Authorization: `Bearer ${token}` })
+      .send();
+    expect(response.status).toEqual(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+
+  it('Should return 401 if the token is invalid', async () => {
+    const token = '';
+    const response = await agent
+      .get('/repoprovas/tests/disciplines')
+      .set({ Authorization: `Bearer ${token}` })
+      .send();
+
+    expect(response.status).toEqual(401);
+  });
+});
+
+afterEach(async () => {
+  await client.$disconnect();
 });
